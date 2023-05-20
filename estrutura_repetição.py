e9117b5a235ab4a36493b396e2ecfa0be33f51b7
Louise Alonso <120966351+louise-alonso@users.noreply.github.com>
2023-05-20 14:38:51 -0300

    Update estrutura_repetição.py

--- estrutura_repetição.py
+++ estrutura_repetição.py
@@ -1,3 +1,5 @@
+                                                         #Lista de exercícios - Estrutura de Repetição:
+
 '''1.Desenvolva um programa que verifique e mostre os números entre 1.000 e 2.000 que, quando divididos por 11, produzam o resto igual a 5.'''
 
 for num in range(1000, 2001):
